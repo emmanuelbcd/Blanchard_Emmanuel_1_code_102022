a9f0ead65386e78c5d3c2e9360eeca141fd02eed
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2022-10-19 23:50:15 +0200

    manipulate dom

--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,37 @@
 //requêter l'Api pour lui demander l'ensemble des produits
-//fetch('http://localhost:3000/api/products')
- // .then((response) => response.json())
-  //.then((json) => console.log(json));
+const response = fetch ("http://localhost:3000/api/products");
 
-async function fetchProducts () {
-  const r = await fetch('http://localhost:3000/api/products', {
-    method: 'GET',
-    headers: {
-      "Accept": "application/json",
-    }
-  }
-  )
-    if (r.ok === true) {
-      return r.json();
-    }
-    throw new Error('Impossible de contacter le serveur')
-}
-
-fetchProducts().then(products => console.log(products))
 
 //récupérer la réponse émise
+  //Récupération de l'élément du DOM qui accueillera les
+  const sectionItems = document.querySelector(".items");
 
-//Parcourir cette réponse pour insérer chaque élément dans la page d'accueil
+
+  
+  //Parcourir cette réponse pour insérer chaque élément dans la page d'accueil
+
+  //Création des éléments et remplissage du texte ou de la source de l'image
+  const article = document.createElement ('article'); 
+
+  const imageElement = document.createElement ('img');
+  imageElement.src = article.image;
+  imageElement.innerText = article.image;
+
+  const nameElement = document.createElement ('h3');
+  nameElement.innerText = article.name;
+
+  const descriptionElement = document.createElement ('p');
+  descriptionElement.innerText = article.description;
+
+  //Ajout des éléments créés dans le DOM
+  sectionItems.appendChild(imageElement);
+  sectionItems.appendChild(nameElement);
+  sectionItems.appendChild(descriptionElement);
+
+
+
+
+
 
 
 
