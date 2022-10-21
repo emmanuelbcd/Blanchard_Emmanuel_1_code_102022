ed6821043aec365caa496ed52b075396d4d0a578
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2022-10-21 22:29:55 +0200

    fetch update

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,24 @@
 //requêter l'Api pour lui demander l'ensemble des produits
-const response = fetch ("http://localhost:3000/api/products");
+
+async function fecthProducts () {
+  const apiRequest = await fetch ("http://localhost:3000/api/products", {
+    method: 'GET',
+    headers: {
+      "Accept" : "application/json"
+    }
+  }
+  )
+  if (apiRequest.ok === true) {
+      return apiRequest.json();
+  }
+  throw new Error ('Impossible de contacter le serveur')
+}
+
+fecthProducts().then(products => console.log(products))
 
 
 //récupérer la réponse émise
-  //Récupération de l'élément du DOM qui accueillera les
+  //Récupération de l'élément du DOM
   const sectionItems = document.querySelector(".items");
 
 
@@ -13,23 +28,30 @@ const response = fetch ("http://localhost:3000/api/products");
   //Création des éléments et remplissage du texte ou de la source de l'image
   const article = document.createElement ('article'); 
 
+  const anchor = document.createElement ('a');
+  anchor.href = "./product.html?id=+article._id"
+
   const imageElement = document.createElement ('img');
-  imageElement.src = article.image;
-  imageElement.innerText = article.image;
+  imageElement.src = article.imageUrl;
+  imageElement.alt = article.altTxt;
 
   const nameElement = document.createElement ('h3');
   nameElement.innerText = article.name;
+  nameElement.setAttribute("class", "productName");
 
   const descriptionElement = document.createElement ('p');
   descriptionElement.innerText = article.description;
+  descriptionElement.setAttribute("class", "productDescription")
 
   //Ajout des éléments créés dans le DOM
-  sectionItems.appendChild(imageElement);
-  sectionItems.appendChild(nameElement);
-  sectionItems.appendChild(descriptionElement);
+  article.append(imageElement);
+  article.append(nameElement);
+  article.append(descriptionElement);
+  anchor.append(article);
+  sectionItems.append(anchor);
 
 
-
+ 
 
 
 
