8010caebfb8526eebce9a292f1f0f0fe396cd87c
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2022-10-24 22:04:34 +0200

    insert products whith a for loop

--- front/js/script.js
+++ front/js/script.js
@@ -16,46 +16,45 @@ async function fecthProducts () {
     }
 }
 
-//fecthProducts
-fecthProducts().then(products => console.log(products))
+//console.table
+fecthProducts().then(products => console.table(products))
 
 // insérer dans la page l'ensemble des produits fournis par l'Api
 async function insertProducts() {
   const result = await fecthProducts()
-  .then ((products) => {
+  .then ((product) => {
 
   
-  //Parcourir cette réponse pour insérer chaque élément dans la page d'accueil
-  for (let product of products) {
+//Parcourir cette réponse pour insérer chaque élément dans la page d'accueil
+for (let i=0; i < product.length; i++) {
       //Récupération de l'élément du DOM
       const sectionItems = document.querySelector(".items");
   
       //Création des éléments et remplissage du texte ou de la source de l'image
-      const article = document.createElement ('article'); 
+      const article = document.createElement ("article"); 
   
-      const anchor = document.createElement ('a');
-      anchor.href = "./product.html?id=+article._id";
-      //anchor.href = "./product.html?id=42";
+      const anchor = document.createElement ("a");
+      anchor.href = `./product.html?id=${product[i]._id}`;
 
-      const imageElement = document.createElement ('img');
-      imageElement.src = article.imageUrl;
-      imageElement.alt = article.altTxt;
+      const imageElement = document.createElement ("img");
+      imageElement.src = product[i].imageUrl;
+      imageElement.alt = product[i].altTxt;
   
-      const nameElement = document.createElement ('h3');
-      nameElement.innerText = article.name;
+      const nameElement = document.createElement ("h3");
+      nameElement.innerText = product[i].name;
       nameElement.setAttribute("class", "productName");
   
-      const descriptionElement = document.createElement ('p');
-      descriptionElement.innerText = article.description;
+      const descriptionElement = document.createElement ("p");
+      descriptionElement.innerText = product[i].description;
       descriptionElement.setAttribute("class", "productDescription");
   
       //Ajout des éléments créés dans le DOM
-      article.append(imageElement);
-      article.append(nameElement);
-      article.append(descriptionElement);
-      anchor.append(article);
-      sectionItems.append(anchor);
-    }  
+      article.appendChild(imageElement);
+      article.appendChild(nameElement);
+      article.appendChild(descriptionElement);
+      anchor.appendChild(article);
+      sectionItems.appendChild(anchor);
+    } ; 
   });    
   console.log("Les produits ont été insérés");
 }
