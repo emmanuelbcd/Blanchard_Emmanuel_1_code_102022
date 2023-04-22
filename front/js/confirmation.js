c366d9753039eebede522783e7309c4fc0e618bb
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-04-22 22:02:57 +0200

    orderId

---
+++ front/js/confirmation.js
@@ -0,0 +1,9 @@
+
+const orderId = document.getElementById("orderId"); //Récupération de l'élément du DOM
+const parsedUrlOrder = new URL(window.location.href);
+const UrlOrderId = parsedUrlOrder.searchParams.get("orderId");
+
+orderId.textContent = UrlOrderId; //on assigne la valeur du numéro de commande à l'élément orderId
+
+console.log("Order Id"); //on affiche le numéro de commande dans la console
+console.log(UrlOrderId);
